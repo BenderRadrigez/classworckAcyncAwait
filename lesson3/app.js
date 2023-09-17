81048e0550918d3e82b06821d026ad3377ac103d
Andrei Petrusevich <danunafi123@gmail.com>
2023-09-17 14:34:30 +0200

    hw-13.09 :first task and add validation

--- lesson3/app.js
+++ lesson3/app.js
@@ -1,19 +1,44 @@
-import {container, name, email, btn, p, URL} from "./scripts/variables.js"
+import { container, name, email, btn, p, URL } from "./scripts/variables.js";
 
 btn.addEventListener("click", () => {
-    fetchData(URL).then((response) => {
-        return response.json();
-    }).then((user) =>{
-        user.length === 0 ? printMessage() : (location.href = "profile.html");
+  fetchData(URL)
+    .then((response) => {
+      return response.json();
     })
+    .then((user) => {
+      user.length === 0 ? printMessage() : (location.href = "profile.html");
+    });
+});
+
+email.addEventListener("keyup", () => {
+    let colorEmail = "";
+    if (!validEmail(email.value)) colorEmail = "red";
+    else colorEmail = "green";
+    email.style.color = colorEmail;
 })
 
-async function fetchData(url){
-    const user = await fetch(`${url}?username=${name.value}&email=${email.value}`);
-    return user;
+async function fetchData(url) {
+  const user = await fetch(
+    `${url}?username=${name.value}&email=${email.value}`
+  );
+  return user;
 }
 
 function printMessage() {
-    p.textContent = "введите правильные данные"
-    container.append(p);
+  p.textContent = "введите правильные данные";
+  container.append(p);
+}
+
+function validEmail(value) {
+  let indexAt = value.search(/\@/g);
+  let indexDot = value.substring(indexAt + 1).search(/\./g);
+  let postfix = value.substring(indexDot + 1);
+
+  return indexAt === -1 || indexDot === -1
+    ? false
+    : value.substring(indexAt + 1, indexDot) < 2
+    ? false
+    : postfix.length < 2 || postfix.search(/\./g) === -1
+    ? false
+    : true;
 }
